7d66b16e8a6a2aca0a7a1c6d92727436945acd4d
Ali Fahrial Anwar <alifahrialanwar@gmail.com>
2022-03-21 16:22:04 +0700

    Update script API OMDB

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 $('#search-button').on('click', function(){
     $.ajax({
-        type: 'get',
-        url:'http://omdbapi.com',
-        dataType: 'json',
+        type: 'get',    //Methode
+        url:'http://omdbapi.com', //url to the api omdb
+        dataType: 'json', //type of data to be returned
         data: {
-            'apikey' : 'e2be7591',
-            's' : $('#search-input').val()
+            'apikey' : 'e2be7591', //sesuaikan api key dengan yang kamu dapatkan dari OMDB
+            's' : $('#search-input').val()  //sesuaikan dengan keyword yang ingin dicari
         },
         success: function(result){
           if(result.Response == 'True'){
